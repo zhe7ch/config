7b2e50296e8ff62f5bd3da56fd279660ffe4923b
zy@freebsd <zhe7ch@gmail.com>
2019-10-25 17:57:26 -0400

    change font to DejaVu

--- st_config.def.h
+++ st_config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font:style=Regular:pixelsize=16:antialias=true:autohint=true";
+static char *font = "DejaVuSansMono Nerd Font:style=Book:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
